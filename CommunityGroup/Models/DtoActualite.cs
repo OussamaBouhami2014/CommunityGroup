f2c8131135a7aa59dc0672db790b77bfe45fe83c
Samir Macbook <macbookpro@new>
2018-12-26 16:48:32 +0100

    Maj

---
+++ CommunityGroup/Models/DtoActualite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+namespace CommunityGroup.Models
+{
+    public partial class DtoActualite
+    {
+        public int ActualiteId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Details { get; set; }
+        public string Introduction { get; set; }
+        public string Image { get; set; }
+        public string CodeUnivers { get; set; }
+        public DateTime DateActualite { get; set; }
+        public DateTime DatePublication { get; set; }
+        public bool IsVue { get; set; }
+    }
+
+    public partial class DtoActualite : BindableObject
+    {
+        public string ColorUnivers { get; set; }
+
+        private string imageActualite = "loading_placeholder.png";
+        public string ImageActualite { get { return imageActualite; } set { imageActualite = value; OnPropertyChanged(); } }
+
+        private Color _actualiteRowColor = Color.FromHex(App.BlueColor);
+        public Color ActualiteRowColor
+        {
+            get
+            {
+                var cl = _actualiteRowColor;
+                return Color.FromRgba(cl.R, cl.G, cl.B, 0.5);
+            }
+            set { _actualiteRowColor = value; }
+        }
+    }
+}
--- CommunityGroup/Views/Actualite/ActualitePage.xaml.cs
+++ CommunityGroup/Views/Actualite/ActualitePage.xaml.cs
@@ -1,17 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using FormsToolkit;
+using CommunityGroup.ViewModels;
 using Xamarin.Forms;
+using CommunityGroup.Models;
 
 namespace CommunityGroup.Views.Actualite
 {
-    public partial class ActualitePage : ContentPage
+    public partial class ActualitePage : BasePage
     {
+        private ActualiteViewModel Vm;
+
         public ActualitePage()
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+            Vm = new ActualiteViewModel();
+            this.BindingContext = Vm;
+        }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ((NavigationPage)Application.Current.MainPage).BarBackgroundColor = Color.FromHex("#F5F5F5");
+            ((NavigationPage)Application.Current.MainPage).BarTextColor = Color.FromHex("#F5F5F5");
+            MessagingService.Current.SendMessage(MessageKeys.ChangeStatutBarColor, "#696969");
+        }
+
+        private void ActualiteTachesListView(object sender, System.EventArgs e)
+        {
+            ActualiteListView.IsRefreshing = false;
+        }
+
+        private async void ActualiteListView_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            var item = ((ListView)sender).SelectedItem as DtoActualite;
+            if (item == null)
+                return;
+
+            //await Application.Current.MainPage.Navigation.PushAsyncSingle(new Views.Actualite.ActualiteDetailPage(item));
+
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
