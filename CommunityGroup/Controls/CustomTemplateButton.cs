e6c3e1ad74e4b541722428e6c7bde31731b4e816
Samir Macbook <macbookpro@new>
2018-12-27 15:48:08 +0100

    Maj

---
+++ CommunityGroup/Controls/CustomTemplateButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CommunityGroup.Controls
+{
+    public class CustomTemplateButton : ContentView
+    {
+        public event EventHandler Clicked;
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var view = sender as CustomTemplateButton;
+            AppsHelper.EffectClick(view);
+            Clicked?.Invoke(sender, e);
+        }
+
+        public CustomTemplateButton()
+        {
+            this.VerticalOptions = LayoutOptions.Center;
+            this.HorizontalOptions = LayoutOptions.Center;
+
+            TapGestureRecognizer gestureRecognizer = new TapGestureRecognizer();
+            gestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
+            this.GestureRecognizers.Add(gestureRecognizer);
+        }
+    }
+}
---
+++ CommunityGroup/Views/Story/AgendaPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CommunityGroup.Views.Story
+{
+    public partial class AgendaPage : ContentPage
+    {
+        public AgendaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
